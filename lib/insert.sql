27cbf700bd2ab1bb734b3869196a7249c664f7d0
McCray <tubach75@hotmail.com>
2021-01-06 12:04:09 -0500

    commit tree d391fff69c48296275067b28607c81287e69a348

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 13, "M", "black", "cool", 0);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "M", "brown", "mellow", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Tabitha", 10, "F", "dark brown", "hyper", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 5, "M", "brown", "intense", 0);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Melissa", 9, "F", "black", "playful", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Grinch", 7, "M", "brown", "sly", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Wendy", 11, "F", "dark brown", "mean", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (null, 8, "M", "brown", "aggressive", 0);
